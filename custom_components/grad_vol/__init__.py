73db2856b240d293aa87dbc26ea6a17f2b612929
NinDTendo <dat.ng.xd@gmail.com>
2025-03-21 00:31:39 +0100

    rewrote integration to be async, now support cancelation of ongoing task based on entity_id. resolves #4

--- custom_components/grad_vol/__init__.py
+++ custom_components/grad_vol/__init__.py
@@ -1,31 +1,73 @@
 DOMAIN = "grad_vol"
-import time
+import asyncio
 
-def setup(hass, config):
+async def async_setup(hass, config):
     """Set up is called when Home Assistant is loading our component."""
+    volume_tasks = {}
 
-    def handle_set_volume(call):
-        """Handle the service call."""
-        entity_ids = call.data.get('entity_id')
-        target_volume= round(float(call.data.get('volume')),2)
-        span = call.data.get('duration',5)
+    async def async_handle_set_volume(call):
+        """Handle the service call for gradually setting volume."""
+        entity_ids = call.data.get('entity_id', [])
+        target_volume = round(float(call.data.get('volume', 0)), 2)
+        span = call.data.get('duration', 5)
+        
         for entity_id in entity_ids:
-            if hass.states.get(entity_id).state ==  'off':
-                continue 
-            volume = round(float(hass.states.get(entity_id).attributes.get('volume_level')),2)
-            if volume == None or abs(volume-target_volume) < 0.02: continue
-            sleeptime = span/(abs(volume - target_volume)/0.01)
-            while abs(volume-target_volume) > 0.02:
+            if entity_id in volume_tasks:
+                volume_tasks[entity_id].cancel()
+            
+            volume_tasks[entity_id] = hass.async_create_task(async_adjust_volume(entity_id, target_volume, span))
+
+    async def async_adjust_volume(entity_id, target_volume, span):
+        """Gradually adjust volume of a media player entity."""
+        try:
+            state = hass.states.get(entity_id)
+            if not state or state.state == 'off':
+                return
+            
+            volume = state.attributes.get('volume_level')
+            if volume is None:
+                return
+            
+            volume = round(float(volume), 2)
+            if abs(volume - target_volume) < 0.02:
+                return
+            
+            steps = abs(int((volume - target_volume) / 0.01))
+            sleeptime = span / max(steps, 1)
+            
+            while abs(volume - target_volume) > 0.02:
+                if entity_id not in volume_tasks:
+                    break  # Task was canceled
+                
                 if target_volume < volume:
                     volume -= 0.01
                 else:
                     volume += 0.01
-                hass.services.call('media_player', 'volume_set', {'entity_id': entity_id, 'volume_level': volume})
-                time.sleep(sleeptime)
-            hass.services.call('media_player', 'volume_set', {'entity_id': entity_id, 'volume_level': target_volume})
-        return
+                
+                # volume = round(volume, 2)
+                await hass.services.async_call('media_player', 'volume_set', {'entity_id': entity_id, 'volume_level': volume})
+                await asyncio.sleep(sleeptime)
+            
+            await hass.services.async_call('media_player', 'volume_set', {'entity_id': entity_id, 'volume_level': target_volume})
+        except asyncio.CancelledError:
+            pass  # Task was cancelled
+        finally:
+            volume_tasks.pop(entity_id, None)
 
-    hass.services.register(DOMAIN, "set_volume", handle_set_volume)
+    async def async_cancel(call):
+        """Cancel the volume adjustment for a specific entity or all."""
+        entity_ids = call.data.get('entity_id', [])
+        if not entity_ids:
+            for task in volume_tasks.values():
+                task.cancel()
+            volume_tasks.clear()
+        else:
+            for entity_id in entity_ids:
+                if entity_id in volume_tasks:
+                    volume_tasks[entity_id].cancel()
+                    volume_tasks.pop(entity_id, None)
 
-    # Return boolean to indicate that initialization was successful.
+    hass.services.async_register(DOMAIN, "set_volume", async_handle_set_volume)
+    hass.services.async_register(DOMAIN, "cancel_all", async_cancel)
+    
     return True
